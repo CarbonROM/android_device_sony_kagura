160b397bbe85fca1ed9cbae4d7a0221bcf781118
Christian Oder <myself5@carbonrom.org>
2017-06-09 10:43:32 +0200

    kagura: Carbonify
    Change-Id: Idd97fdf9a39768dc14f50461ea2d7c9f036c8d40
    
    deps: track default revision
    
    Change-Id: I0feb981bf155231083a1556ca64fe9c38d1525ba

--- device.mk
+++ device.mk
@@ -100,7 +100,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.usb.pid_suffix=1E7
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/sony/tone/platform.mk)
+$(call inherit-product, device/sony/tone-common/platform.mk)
 
 # copy wlan firmware
 $(call inherit-product-if-exists, vendor/broadcom/wlan/bcmdhd/firmware/bcm4359/device-bcm-vendor.mk)
