b72392e4b91cb1761d64966bf5b1cf4affd2442b
Christian Oder <myself5@carbonrom.org>
2017-06-09 10:43:32 +0200

    kagura: Carbonify
    Change-Id: Idd97fdf9a39768dc14f50461ea2d7c9f036c8d40
    
    deps: track default revision
    
    Change-Id: I0feb981bf155231083a1556ca64fe9c38d1525ba

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/tone/PlatformConfig.mk
+include device/sony/tone-common/PlatformConfigCarbon.mk
+include device/sony/tone-common/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := F8331
 
